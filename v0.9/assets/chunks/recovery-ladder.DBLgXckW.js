140a4cb1af2048e453de3f03c1a402be4bef3bc2
Documenter.jl <documenter@juliadocs.github.io>
2026-09-12 02:28:24 +0000

    build based on b1f8f14

---
+++ v0.9/assets/chunks/recovery-ladder.DBLgXckW.js
@@ -0,0 +1,1 @@
+const s="/HSquared.jl/v0.9/assets/recovery-ladder.CEG4WwY-.svg";export{s as _};
--- versions.js
+++ versions.js
@@ -1,5 +1,7 @@
 var DOC_VERSIONS = [
   "dev",
   "stable",
+  "v0.9",
 ];
+var DOCUMENTER_NEWEST = "0.9.0";
 var DOCUMENTER_STABLE = "stable";
